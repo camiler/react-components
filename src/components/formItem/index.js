1b94d5fe5bbe726a1159286384bc0f252e4b9d18
camiler <1159724006@qq.com>
2019-01-15 14:55:00 +0800

    formitem hook

--- src/components/formItem/index.js
+++ src/components/formItem/index.js
@@ -1,68 +1,57 @@
-import React, {Component} from 'react';
+import React, {useState, useEffect} from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
 
 import './formItem.less';
 
-class FormItem extends Component {
+const FormItem = (props) => {
+  const [errorText, setErrorText] = useState('');
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: '',
-      errorText: '',
-    };
+  const setError = (errorText) => {
+    setErrorText(errorText);
   }
 
-  setError = (errorText) => {
-    this.setState({errorText})
-  }
-
-  renderChildren = (children) => {
-    const {rule, getValue, idFor} = this.props;
+  const renderChildren = (children) => {
+    const {rule, getValue, idFor} = props;
     return React.Children.map(children, (child) => {
       if (child && typeof child.type === 'function' && !child.props.size) {
-        return React.cloneElement(child, {rule, getValue, setError: this.setError, id: idFor});
+        return React.cloneElement(child, {rule, getValue, setError, id: idFor});
       }
       return child;
     });
   }
 
-  render() {
-    const {errorText} = this.state;
-
-    const {label, idFor, children, labelWidth, disabled, border, horizontal} = this.props;
-    if (!label) {
-      return (
-        <div className={classnames('form-item', {'bor-bottom1px': border}, {disabled})}>
-          {this.renderChildren(children)}
-        </div>
-      )
-    }
+  const {label, idFor, children, labelWidth, disabled, border, horizontal} = props;
+  if (!label) {
     return (
       <div className={classnames('form-item', {'bor-bottom1px': border}, {disabled})}>
-        {
-          horizontal ? (
-            <div className="item-control flex">
-              <label htmlFor={idFor} className="item-label" style={{width: labelWidth}}>{label}</label>
-              <div className="flexBox item-wrap">
-                {this.renderChildren(children)}
-                {errorText && <p className="item-info flex"><i className="icon icon-error-info"></i><span className="flexBox">{errorText}</span></p>}
-              </div>
-            </div>
-          ) : (
-            <div>
-              <label htmlFor={idFor} className="item-label" style={{width: '100%'}}>{label}</label>
-              <div className="flex item-wrap">
-                {this.renderChildren(children)}
-              </div>
-              {errorText && <p className="item-info flex"><i className="icon icon-error-info"></i><span className="flexBox">{errorText}</span></p>}
-            </div>
-          )
-        }
+        {renderChildren(children)}
       </div>
     )
   }
+  return (
+    <div className={classnames('form-item', {'bor-bottom1px': border}, {disabled})}>
+      {
+        horizontal ? (
+          <div className="item-control flex">
+            <label htmlFor={idFor} className="item-label" style={{width: labelWidth}}>{label}</label>
+            <div className="flexBox item-wrap">
+              {renderChildren(children)}
+              {errorText && <p className="item-info flex"><i className="icon icon-error-info"></i><span className="flexBox">{errorText}</span></p>}
+            </div>
+          </div>
+        ) : (
+          <div>
+            <label htmlFor={idFor} className="item-label" style={{width: '100%'}}>{label}</label>
+            <div className="flex item-wrap">
+              {renderChildren(children)}
+            </div>
+            {errorText && <p className="item-info flex"><i className="icon icon-error-info"></i><span className="flexBox">{errorText}</span></p>}
+          </div>
+        )
+      }
+    </div>
+  )
 }
 
 FormItem.propTypes = {
